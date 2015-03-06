933881e0e700e5837daefbf9d653b2f32bafc33a
Mathew Mendelsohn <=>
2015-03-06 05:09:44 -0500

    Ongoing Ramp climbing

--- Main_Program/Main_Program.ino
+++ Main_Program/Main_Program.ino
@@ -8,8 +8,9 @@ const int BaseWidth = 30; // cm, 1 foot
 const int CourseWidth = 300; // cm, 3m
 const int CourseLength = 600; //cm, 6m
 const int SafteyFactor = 2;
-const float North = 90.0;
-const float South = 270.0;
+const float North = 270.0;
+const float NorthEast = 297.5;
+const float South = 90.0;
 const float East = 0;
 const float West = 180.0;
  
@@ -26,6 +27,13 @@ const int RightMediumForward = 1500;
 //Fast Forward
 const int LeftFastForward = 1160;
 const int RightFastForward = 1535;
+
+//IR
+const int LeftIRLimit = 110;
+const int RightIRLimit = 60;
+
+const int FlangeDown = 135;
+const int FlangeUp = 0;
  
  
 LSM303 compass;
@@ -47,11 +55,11 @@ int servoFlangePin = 7;
 Servo servoFlange;
 
 int bumpTopPin = 18;
-int bumpTopInterrupt = 4;
+int bumpTopInterrupt = 5;
 volatile boolean bumpTop = false; // Triggered true by boolean. 
 
 int bumpBottomPin = 19;
-int bumpBottomInterrupt = 5;
+int bumpBottomInterrupt = 4;
 volatile boolean bumpBottom = false; // Triggered true by boolean. 
 
 int IRLeftPin = A0;
@@ -94,6 +102,84 @@ void keepForward(int leftSpeed, int rightSpeed, float straightHeading)
   }
 }
 
+void forward(int velPercent, float wantedHeading)
+
+{
+
+   // Normalize velocity{0..99}, heading{0..360}
+
+   velPercent %= 100;
+
+   while (wantedHeading < 0.0) wantedHeading += 360;
+
+   while (wantedHeading >= 360) wantedHeading -= 360;
+
+   
+
+   compass.read();
+
+   float _heading = compass.heading();
+
+   //while (_heading < 0.0) _heading += 360;
+
+   //while (_heading >= 360) _heading -= 360;
+
+   
+
+   float adjustAngle = wantedHeading - _heading;
+
+   while (adjustAngle > 180) adjustAngle -= 360;
+
+   while (adjustAngle < -180) adjustAngle += 360;
+
+   
+
+   int leftRange = 1378-1160;
+
+   int rightRange = 1535-1336;
+
+   
+
+   int leftSpeed = (velPercent*leftRange/100);
+
+   int rightSpeed = (velPercent*rightRange/100);
+
+  
+
+   if (adjustAngle > 0) 
+
+   {
+
+      leftSpeed = leftSpeed + (leftRange - leftSpeed)*(adjustAngle)/180.0;
+
+      rightSpeed = rightSpeed - rightSpeed*(adjustAngle)/180.0;
+
+   }
+
+   else
+
+   {
+
+      leftSpeed = leftSpeed - leftSpeed*(-adjustAngle)/180.0;
+
+      rightSpeed = rightSpeed + (rightRange - rightSpeed)*(-adjustAngle)/180.0;
+
+   }
+
+   
+
+   leftSpeed = 1378 - leftSpeed;
+
+   rightSpeed = 1336 + rightSpeed;
+
+   
+
+   servoLeft.writeMicroseconds(leftSpeed);
+
+   servoRight.writeMicroseconds(rightSpeed);
+
+}
+
 void brake(){
     servoLeft.writeMicroseconds(1378);  
     servoRight.writeMicroseconds(1336);    
@@ -120,7 +206,7 @@ void turn(float offset)
   compass.read();
   heading = compass.heading();
   float wantedHeading = initialHeading + offset;
-  
+
   // This algorithm determines whether it is faster to turn right or left depending on the current heading.
   if(heading < initialHeading)
   {
@@ -168,13 +254,22 @@ boolean headingCheck(double offset)  // Checks that the heading is correct.
   {
     temp = true;
   }
+  
   return temp;
 }
 
-boolean onRamp() // Checks if on ramp.
+boolean onRamp(double percent) // Checks if on ramp.
 {  
-  compass.read();
-  if(onRampGravity < compass.a.z)
+  float gravity = 0;
+  for (int i = 0; i < 5; i ++)
+  {
+    compass.read();
+    gravity += compass.a.z;
+    delay(20);
+  }
+  
+  gravity /= 5;
+  if(initialGravity * percent < gravity)
   {
     return true;
   }  
@@ -187,24 +282,24 @@ void stayOnRamp()
   int leftIRValue = analogRead(IRLeftPin);   
   int rightIRValue = analogRead(IRRightPin); 
   
-  if( (leftIRValue < 80)&&(rightIRValue < 80)) // Check left IR. Change to correct calibration cutoff.
+  if( (leftIRValue < LeftIRLimit)&&(rightIRValue < RightIRLimit)) // Check left IR. Change to correct calibration cutoff.
   {
     // Add more complicated course adjustment.
-    forward();
+    forward(LeftSlowForward, RightSlowForward + 50);  
   }
-  if( leftIRValue < 80 ) // Check left IR. Change to correct calibration cutoff.
+  else if( leftIRValue < LeftIRLimit ) // Check left IR. Change to correct calibration cutoff.
   {
     // Add more complicated course adjustment.
     right();
   }
-  else if( rightIRValue < 80 ) // Check Right IR. Change to correct calibration cutoff.
+  else if( rightIRValue < RightIRLimit ) // Check Right IR. Change to correct calibration cutoff.
   {
     // Add more complicated course adjustment.
     left();
   }
   else
   {
-    forward();
+    forward(LeftSlowForward, RightSlowForward + 50);
   }
 }
 
@@ -335,6 +430,10 @@ void setup()
   servoArm.attach(servoArmPin);
   servoFlange.attach(servoFlangePin);
   
+  servoArm.write(0);
+  servoFlange.write(FlangeUp);
+  brake();
+  
   pinMode(IRLeftPin, INPUT);
   pinMode(IRRightPin, INPUT);
   
@@ -345,65 +444,126 @@ void setup()
   attachInterrupt(bumpTopInterrupt, ISR_BUMP_TOP, CHANGE); // Note- can be LOW, CHANGE, RISING, or FALLING
   attachInterrupt(bumpBottomInterrupt, ISR_BUMP_BOTTOM, CHANGE); // Note- can be LOW, CHANGE, RISING, or FALLING
   
+  while(!bumpTop);
+    
   lidar.initPWM(lidarTriggerPin, lidarMonitorPin);
   
   Wire.begin();
   compass.init();
   compass.enableDefault();  
-  compass.m_min = (LSM303::vector<int16_t>){  -1148,   -681,  -3836};
-  compass.m_max = (LSM303::vector<int16_t>){  +1418,  +1286,  -1843};
+  compass.m_min = (LSM303::vector<int16_t>){  -2613,   -2248,  +2484};
+  compass.m_max = (LSM303::vector<int16_t>){  +1015,  +1103,  +3069};
   
+  delay(100);
   compass.read();  
   initialHeading = compass.heading();
   initialGravity = compass.a.z;
-  onRampGravity = initialGravity*0.72; // This should be 0.707*z at 45 degrees;
+  onRampGravity = initialGravity*0.80; // This should be 0.707*z at 45 degrees;
   
-  //Serial.begin(9600);
-  // Sets the servos to an initial position so that it does not move at start up.
-  servoArm.write(0);
-  servoFlange.write(0);
-  brake();
-  delay(500);  
+  Serial.begin(9600);
+  // Sets the servos to an initial position so that it does not move at start up 
+  delay(500);
 } 
 
 void loop() 
 {
+
   // Forward unitl hitting bump sensor.
-  moveUntilBump();
+ // moveUntilBump();
   
   // Turn until the right heading
-  turn(90.0);
+  //turn(NorthEast);
+  forward(LeftFastForward, RightMediumForward);
   
   // Add course corrections as needed.
   
   // Drive forward until on ramp.
-  forward();  
-  while(!(onRamp()))
+  //forward();  
+  //forward(LeftSlowForward, RightSlowForward);
+  while(!(onRamp(0.8)))
   {
-  delay(10);
-  } 
+    delay(100);
+  }
+
+  forward(LeftMediumForward, RightMediumForward);
+  delay(1000); 
+
+  //forward(LeftSlowForward - 50, RightSlowForward);
+  //while(analogRead(IRRightPin) > RightIRLimit );
+  //forward(LeftSlowForward, RightSlowForward);
+  //delay(200);
+  servoFlange.write(FlangeDown);
+
+  //left();
+  //delay(5);
   
+  //forward(LeftSlowForward, LeftSlowForward + 50);
+  //while(analogRead(IRRightPin) < RightIRLimit);
+ 
+  forward(LeftMediumForward, RightMediumForward);
+  
+  delay(250);
+  
+  forward(LeftSlowForward, RightSlowForward + 25);
+
+  delay(2000);  
   // Initial up ramp
-  while(onRamp())
+  /*while(onRamp(0.9))
   {
-    stayOnRamp();
-    delay(10);
+//    stayOnRamp();
+  }
+  */
+  
+  while(analogRead(IRRightPin) > RightIRLimit && analogRead(IRLeftPin) > LeftIRLimit);
+  
+  compass.read();
+  float yAcceleration = compass.a.y;
+  
+  //forward(LeftSlowForward, RightSlowForward + 50);
+   forward(LeftFastForward, RightFastForward);
+
+  delay(2500);
+  
+  forward(LeftFastForward, RightFastForward);
+
+  for(int i = 10; i <=65; i++)
+  {
+    servoArm.write(i);
+    delay(60);
   }
   
-  // At the corner it drives straight over.
-  forward();
-  while(!onRamp())
+  delay(200);
+  
+  forward(LeftSlowForward, RightSlowForward + 25);
+  
+  do
+  {
+    compass.read();
+  }
+  while(-0.75 * yAcceleration < compass.a.y);
+  
+ /* 
+  while(!onRamp(0.85))
   {
   delay(10);
   } 
+*/  
+  //forward(LeftStop + 100, RightStop - 50);
   
+  delay(2000);
+  servoArm.write(10);
+
   // Going down ramp
-  while(onRamp())
+  while(onRamp(0.9))
   {
-    stayOnRamp();
+ //   stayOnRamp();
   }  
-  brake(); 
+  servoFlange.write(FlangeUp);
   
+  while(onRamp(0.99));  
+  
+  brake(); 
+  /*
   // Search find and capture lego figure
   gridSearch();
   
@@ -435,7 +595,7 @@ void loop()
   turn(west);  // Line up with base.  
   moveUntilBump();
   deployTrap(); // Safely deposit person at base 2.  
-  
+  */
   while(1) // do nothing after the main loop.
   {
   }
@@ -446,7 +606,10 @@ void ISR_BUMP_TOP()
   // Double check that arduinon automaticaly resets the register for me.
   
   //Pin is normally High, Low when button pressed
+  Serial.println("TOP");
+  Serial.println(digitalRead(bumpTopPin));
   bumpTop = (digitalRead(bumpTopPin) == LOW);
+  Serial.println(bumpTop);
 }
 
 void ISR_BUMP_BOTTOM()
@@ -454,5 +617,6 @@ void ISR_BUMP_BOTTOM()
   // Double check that arduinon automaticaly resets the register for me.
   
   //Pin is normally High, Low when button pressed
+  Serial.println("bottom");
   bumpBottom = (digitalRead(bumpBottomPin) == LOW);
 }
